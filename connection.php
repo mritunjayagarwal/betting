92cffd21a7709370c0cc92b60d95f6d45f10683b
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2022-06-03 21:35:43 +0530

    gg

--- connection.php
+++ connection.php
@@ -7,7 +7,11 @@ $connect=mysqli_connect($host,$databasebUser,$databasePwd,$dataBaseName);
 $name = $_GET['name'];
 $age = $_GET['contact'];
 $country = $_GET['email'];
-
+if($connect==true){
+    echo "connected";
+}else{
+   echo" not connected";
+};
 $myQuery = "INSERT INTO tb_data VALUES('$name', '$age', '$country')";
 $runQuery= mysqli_query($connect, $myQuery);
 
